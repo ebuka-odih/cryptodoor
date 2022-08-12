ef9b3e477ce81b53db297656df068d3eee196806
Gnosis <gnosis@Gnosis-MacBook-Pro.local>
2022-08-12 07:31:19 +0100

    commit changes

--- resources/views/pages/index4.blade.php
+++ resources/views/pages/index4.blade.php
@@ -321,11 +321,11 @@
                     <div class="box featured" data-aos="zoom-in" data-aos-delay="200"><h3>Business</h3><h4><sup>$</sup>35<span> </span>
                         </h4>
                         <ul>
-                            <li>Aida dere</li>
-                            <li>Nec feugiat nisl</li>
-                            <li>Nulla at volutpat dola</li>
-                            <li>Pharetra massa</li>
-                            <li class="na">Massa ultricies mi</li>
+                            <li>-Get Full Access ( Become a Member).</li>
+                            <li>-Get Access to All Resources and also Professional Coaching</li>
+                            <li>- Access to All Crypto Event Worldwide</li>
+                            <li>- Hotel Reservation and Assistance</li>
+                            <li class="na">- Support and Assistance with all Crypto/Tech Needs</li>
                         </ul>
                         <div class="btn-wrap"><a href="#" class="btn-buy">Buy Now</a></div>
                     </div>
@@ -334,11 +334,11 @@
                     <div class="box" data-aos="zoom-in" data-aos-delay="300"><h3>Developer</h3><h4><sup>$</sup>145<span></span>
                         </h4>
                         <ul>
-                            <li>Aida dere</li>
-                            <li>Nec feugiat nisl</li>
-                            <li>Nulla at volutpat dola</li>
-                            <li>Pharetra massa</li>
-                            <li>Massa ultricies mi</li>
+                            <li>-Get Full Access ( Become a Member).</li>
+                            <li>-Get Access to All Resources and also Professional</li>
+                            <li>-Get Crypto Jobs , Work with the Team.</li>
+                            <li>-Easy Flight Reservations for Crypto Events Worldwide</li>
+                            <li>-Exclusive Hotels Options and Support.</li>
                         </ul>
                         <div class="btn-wrap"><a href="#" class="btn-buy">Buy Now</a></div>
                     </div>
@@ -347,15 +347,77 @@
                     <div class="box" data-aos="zoom-in" data-aos-delay="400"><span class="advanced">Advanced</span>
                         <h3>Ultimate</h3><h4><sup>$</sup>590<span> </span></h4>
                         <ul>
-                            <li>Aida dere</li>
-                            <li>Nec feugiat nisl</li>
-                            <li>Nulla at volutpat dola</li>
-                            <li>Pharetra massa</li>
-                            <li>Massa ultricies mi</li>
+                            <li>-Get Full Access ( Become a Member).</li>
+                            <li>-Get Access to All Resources and also Professional Coaching</li>
+                            <li>-Access To Exclusive Crypto Jobs</li>
+                            <li>-Access to VIPs Zooms and Meetings</li>
+                            <li>-VIP Hotel and Flights Reservation Options for all Crypto Events.</li>
+                            <li>-Top Priority Trading.</li>
                         </ul>
                         <div class="btn-wrap"><a href="#" class="btn-buy">Buy Now</a></div>
                     </div>
                 </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="faq" class="faq section-bg">
+        <div class="container">
+            <div class="section-title aos-init aos-animate" data-aos="fade-up"><h2>F.A.Q</h2>
+                <p>Frequently Asked Questions</p></div>
+            <div class="faq-list">
+                <ul>
+                    <li data-aos="fade-up" class="aos-init aos-animate"><i class="bx bx-help-circle icon-help"></i> <a
+                            data-bs-toggle="collapse" class="collapse" data-bs-target="#faq-list-1">Non consectetur a
+                            erat nam at lectus urna duis? <i class="bx bx-chevron-down icon-show"></i><i
+                                class="bx bx-chevron-up icon-close"></i></a>
+                        <div id="faq-list-1" class="collapse show" data-bs-parent=".faq-list"><p> Feugiat pretium nibh
+                                ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida.
+                                Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non.</p></div>
+                    </li>
+                    <li data-aos="fade-up" data-aos-delay="100" class="aos-init aos-animate"><i
+                            class="bx bx-help-circle icon-help"></i> <a data-bs-toggle="collapse"
+                                                                        data-bs-target="#faq-list-2" class="collapsed">Feugiat
+                            scelerisque varius morbi enim nunc? <i class="bx bx-chevron-down icon-show"></i><i
+                                class="bx bx-chevron-up icon-close"></i></a>
+                        <div id="faq-list-2" class="collapse" data-bs-parent=".faq-list"><p> Dolor sit amet consectetur
+                                adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec
+                                ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est
+                                pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa
+                                tincidunt dui.</p></div>
+                    </li>
+                    <li data-aos="fade-up" data-aos-delay="200" class="aos-init aos-animate"><i
+                            class="bx bx-help-circle icon-help"></i> <a data-bs-toggle="collapse"
+                                                                        data-bs-target="#faq-list-3" class="collapsed">Dolor
+                            sit amet consectetur adipiscing elit? <i class="bx bx-chevron-down icon-show"></i><i
+                                class="bx bx-chevron-up icon-close"></i></a>
+                        <div id="faq-list-3" class="collapse" data-bs-parent=".faq-list"><p> Eleifend mi in nulla
+                                posuere sollicitudin aliquam ultrices sagittis orci. Faucibus pulvinar elementum integer
+                                enim. Sem nulla pharetra diam sit amet nisl suscipit. Rutrum tellus pellentesque eu
+                                tincidunt. Lectus urna duis convallis convallis tellus. Urna molestie at elementum eu
+                                facilisis sed odio morbi quis</p></div>
+                    </li>
+                    <li data-aos="fade-up" data-aos-delay="300" class="aos-init aos-animate"><i
+                            class="bx bx-help-circle icon-help"></i> <a data-bs-toggle="collapse"
+                                                                        data-bs-target="#faq-list-4" class="collapsed">Tempus
+                            quam pellentesque nec nam aliquam sem et tortor consequat? <i
+                                class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+                        <div id="faq-list-4" class="collapse" data-bs-parent=".faq-list"><p> Molestie a iaculis at erat
+                                pellentesque adipiscing commodo. Dignissim suspendisse in est ante in. Nunc vel risus
+                                commodo viverra maecenas accumsan. Sit amet nisl suscipit adipiscing bibendum est. Purus
+                                gravida quis blandit turpis cursus in.</p></div>
+                    </li>
+                    <li data-aos="fade-up" data-aos-delay="400" class="aos-init aos-animate"><i
+                            class="bx bx-help-circle icon-help"></i> <a data-bs-toggle="collapse"
+                                                                        data-bs-target="#faq-list-5" class="collapsed">Tortor
+                            vitae purus faucibus ornare. Varius vel pharetra vel turpis nunc eget lorem dolor? <i
+                                class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+                        <div id="faq-list-5" class="collapse" data-bs-parent=".faq-list"><p> Laoreet sit amet cursus sit
+                                amet dictum sit amet justo. Mauris vitae ultricies leo integer malesuada nunc vel.
+                                Tincidunt eget nullam non nisi est sit amet. Turpis nunc eget lorem dolor sed. Ut
+                                venenatis tellus in metus vulputate eu scelerisque.</p></div>
+                    </li>
+                </ul>
             </div>
         </div>
     </section>
