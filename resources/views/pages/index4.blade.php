a298079182f55edc1dbd4510da7ba26a5b62e05d
Gnosis <gnosis@Gnosis-MacBook-Pro.local>
2022-08-23 13:06:03 +0100

    commit changes

--- resources/views/pages/index4.blade.php
+++ resources/views/pages/index4.blade.php
@@ -32,11 +32,11 @@
             color: white;
         }
 
-        .about2 .video-box {
-            background: url(../img2/flyer.jpg) center center no-repeat;
-            background-size: contain;
-            min-height: 300px;
-        }
+        /*.about2 .video-box {*/
+        /*    background: url(../img2/flyer.jpg) center center no-repeat;*/
+        /*    background-size: contain;*/
+        /*    min-height: 300px;*/
+        /*}*/
 
     </style>
 
@@ -97,11 +97,11 @@
     </svg>
 </section>
 <main id="main">
-    <section id="about" class="about2">
+    <section id="about" class="about">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-xl-5 col-lg-6 video-box d-flex justify-content-center align-items-stretch"
-                     data-aos="fade-right"><a href="https://www.youtube.com/watch?v=StpBR2W8G5A"
+                     data-aos="fade-right"><a href="https://youtube.com/shorts/06K-PCUsshg"
                                               class="glightbox play-btn mb-4"></a></div>
                 <div
                     class="col-xl-7 col-lg-6 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-5"
